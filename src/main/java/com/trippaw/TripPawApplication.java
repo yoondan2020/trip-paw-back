7f7c8ae7dc26bea59475ac093a50101ab68303e0
yoondan2020 <yoondan2020@naver.com>
2026-09-03 16:10:27 +0900

    chore: 개발 환경 및 Git 설정 정리

--- src/main/java/com/trippaw/TripPawApplication.java
+++ src/main/java/com/trippaw/TripPawApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TripPawApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(TripPawApplication.class, args);
     }
