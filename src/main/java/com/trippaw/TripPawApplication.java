9ba166145624180f706b610a5895bd123eddaf2a
yoondan2020 <yoondan2020@naver.com>
2026-09-03 15:34:11 +0900

    feat: 백엔드 프로젝트 초기 설정

---
+++ src/main/java/com/trippaw/TripPawApplication.java
@@ -0,0 +1,13 @@
+package com.trippaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripPawApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trippaw/TripPawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trippaw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripPawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
